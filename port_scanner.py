573a9c4ec7ee7bc19ec394e4e8ef691811b4e0b7
waleska <paulaboyanoivars@gmail.com>
2020-08-15 18:36:16 +0200

    port scanner finished

--- port_scanner.py
+++ port_scanner.py
@@ -8,23 +8,33 @@ class Scanner:
 
     def __repr__(self):
         return 'Scanner: {}'.format(self.ip)
+    
+    def add_port(self, port):
+        self.open_ports.append(port)
 
     def scan(self, lowerport, upperport):
-        pass
+        for port in range(lowerport, upperport + 1):
+            if self.is_open(port):
+                self.add_port(port)
+
 
     def is_open(self, port):
-        pass
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = s.connect_ex((self.ip, port))
+        s.close()
+        return result == 0
 
     def write(self, filepath):
-        pass
+        openport = map(str, self.open_ports)
+        with open(filepath, 'w') as f:
+            f.write('\n'.join(openport))
 
 
 def main():
     ip = '192.168.1.38'
     scanner = Scanner(ip)
-    print(repr(scanner))
-
-
+    scanner.scan(1, 100)
+    scanner.write('./open_ports')
 
 
 if __name__ == '__main__':
